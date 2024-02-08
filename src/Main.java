f5a02ab8c5986973ce1daf04a525a67b17b8ac32
Dhanush Kumar <dk652003@gmail.com>
2024-02-08 17:41:06 +0530

    Implemented All The Password Valizator Condition

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,9 @@ public class Main {
     public static void main(String[] args) {
         String Password = "bcde45A@Bfg";
         //.* will allow any sequence of character
-        String Passwordregex = "(?=.*[0-9])(?=.*[A-Z])[A-Za-z0-9]*[\\@\\$\\%][A-Za-z0-9]*";
+        String Passwordregex = "(?=.*[0-9])(?=.*[A-Z])[A-Za-z0-9]*[\\@\\$\\%\\!\\#\\^\\&\\*\\(\\)\\-\\+][A-Za-z0-9]*";
         Check pass = new Check(Password, Passwordregex);
         pass.passwordvalidator();
+
     }
 }
