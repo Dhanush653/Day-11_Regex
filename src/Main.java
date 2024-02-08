e3d8157fbb4853e4ddd30fe1c047a648fc4cea2b
Dhanush Kumar <dk652003@gmail.com>
2024-02-08 11:38:51 +0530

    Added Validation of Firstname Using Regex

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import com.bridgelabz.regex.Check;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String firstname = "Paul";
+        String firstnameregex = "[A-Z][a-zA-Z]{2,}";
+
+        Check name = new Check(firstname, firstnameregex);
+        name.firstname();
     }
 }
--- src/com/bridgelabz/regex/Check.java
+++ src/com/bridgelabz/regex/Check.java
@@ -1,4 +1,25 @@
 package com.bridgelabz.regex;
-
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 public class Check {
+    String val;
+    String check;
+
+    public Check(String val, String check) {
+        this.val = val;
+        this.check = check;
+    }
+    public void firstname(){
+        Pattern pattern = Pattern.compile(check);
+        Matcher matcher = pattern.matcher(val);
+
+        if(matcher.matches())
+        {
+            System.out.println("Valid");
+        }
+        else
+            System.out.println("Not Valid");
+
+    }
 }
+
