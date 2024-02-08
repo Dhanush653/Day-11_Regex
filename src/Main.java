67e709f0795a2e09659dcdeb03d829c91997fc8a
Dhanush Kumar <dk652003@gmail.com>
2024-02-08 16:42:48 +0530

    Added Password Rule-3

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,9 @@ import com.bridgelabz.regex.Check;
 
 public class Main {
     public static void main(String[] args) {
-        String Password = "Abcdefghi";
-        String Passwordregex = "(?=[A-Z])[A-Za-z0-9\\@\\$\\%]{8,}";
+        String Password = "bcd98eABfg";
+        //.* will allow any sequence of character
+        String Passwordregex = "(?=.*[0-9])(?=.*[A-Z])[A-Za-z0-9\\@\\$\\%]{8,}";
         Check pass = new Check(Password, Passwordregex);
         pass.passwordvalidator();
     }
