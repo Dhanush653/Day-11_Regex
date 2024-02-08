f1fa0b0aa6ac7252fa48e532eee8fd211986ac0f
Dhanush Kumar <dk652003@gmail.com>
2024-02-08 17:10:06 +0530

    Added Password Rule4

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ import com.bridgelabz.regex.Check;
 
 public class Main {
     public static void main(String[] args) {
-        String Password = "bcd98eABfg";
+        String Password = "bcde45A@Bfg";
         //.* will allow any sequence of character
-        String Passwordregex = "(?=.*[0-9])(?=.*[A-Z])[A-Za-z0-9\\@\\$\\%]{8,}";
+        String Passwordregex = "(?=.*[0-9])(?=.*[A-Z])[A-Za-z0-9]*[\\@\\$\\%][A-Za-z0-9]*";
         Check pass = new Check(Password, Passwordregex);
         pass.passwordvalidator();
     }
